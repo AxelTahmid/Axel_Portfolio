c771b466ec8d6d98f4fa62c93c35515c5fb69825
AxelTamid <axel.tahmid@gmail.com>
2022-05-11 15:31:29 +0600

    experience update

--- resources/views/components/skills.blade.php
+++ resources/views/components/skills.blade.php
@@ -2,38 +2,38 @@
  <section id="skills" class="section">
     <h2 class="title">SKILLS</h2>
     <div class="section-des">
-If you define a problem correctly, you almost have the solution.  - Steve Jobs
+    If you define a problem correctly, you almost have the solution.  - Steve Jobs
     </div>
     <p class="content-670">
-        Viverra tristique placerat in sat massa consectetur quisque. Nunc aces fames lectus in libero aliquet ertare feraso est lorem nunc dolor sit amet settera aces volutpat in voltare lupar est phasellus. Faucibus sed tristique fames sed aliquet ultricies eget viverra arcu.
+        I am well-seasoned in development of E-commerce, Portfolio and Software as a Service (Saas) applications. My experience in SaaS applications has sharpened my skills and contributed to being a decent developer.
     </p>
 
     <ul class="skills-history block-right">
         <li>
-            <span class="date">2016</span>
+            <span class="date">2020</span>
             <p>
-                <strong>2015 - 2016</strong> placerat in massa consectetur quisque. Nunc ac fames lectus in libero aliquet est setera nunc.
+                <a target="_blank" href="https://esscre.com" style="color: white;">Esscre</a> is a platform for creative professionals. I kickstarted my career here and developed in React, Laravel & Wordpress. Currently responsible for maintaining the project.
             </p>
         </li>
 
         <li>
-            <span class="date">2017</span>
+            <span class="date">2021</span>
             <p>
-                <strong>2016 - 2017</strong> Turpis metus sit diam purus leo in variusquam. Nunc amet tristique volutpat adipiscing vulputate phasellus. Eget enim aliquam euismod mauris in vitae viverra blandit lectus diam feugiat.
+                <a target="_blank" href="http://ebexsoft.com/" style="color: white;">Ebexsoft</a> is a traditional IT company focused on ERP solutions catering to local markets. I got hands on experience with SaaS applications for the first time while working here.
             </p>
         </li>
 
         <li>
-            <span class="date">2019</span>
+            <span class="date">2021</span>
             <p>
-                <strong>2020 - 2021</strong> Nunc amet tristique volutpat adipiscing vulputate phasellus. Eget enim aliquam euismod.
+                <a target="_blank" href="https://jumpstartbd.com" style="color: white;">JumpStart</a>  is a platform for creative funding. This is a Platform as a Service (PaaS) that I have built using WordPress. I am  one of the co-founders. 
             </p>
         </li>
 
         <li>
-            <span class="date">2023</span>
+            <span class="date">2022</span>
             <p>
-                <strong>2022 - 2026</strong> Eget enim aliquam euismod mauris in vitae viverra blandit lectus faucibus diam consequat maecenas turpis metus sit diam purus leo in varius.
+                <a target="_blank" href="http://tikweb.dk" style="color: white;">Tikweb</a> provides tech teams to startups to build their SaaS perfect – every time. I am currently employed here, developing SaaS.
             </p>
         </li>
     </ul>
