b8f914632bc2c33a78b24a1c9602f1a1e415fffa
Axel Tahmid <axel.tahmid@gmail.com>
2022-05-11 23:07:22 +0600

    cover change

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 <section id="home" class="section">
     <div class="container-wrap">                        
         <h1 class="entry-title" data-jarallax-element="0 30">I’m <span>Shahadat Hossain</span></h1>
-        <img class="main-photo block-right" data-jarallax-element="0 -30" src="images/home.jpg" alt="">                        
+        <img class="main-photo block-right" data-jarallax-element="0 -30" src="images/home.png" alt="">                        
         <a href="#services" class="main-btn slow-scroll"><img src="images/scroll-icon.svg" alt=""></a>
     </div>
 </section>
