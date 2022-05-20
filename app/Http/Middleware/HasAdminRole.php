8fb9587b4b187e23494515207e8120b12d67b5c6
Axel Tahmid <axel.tahmid@gmail.com>
2022-05-21 01:06:49 +0600

    htaccess auth header change

--- app/Http/Middleware/HasAdminRole.php
+++ app/Http/Middleware/HasAdminRole.php
@@ -16,11 +16,13 @@ class HasAdminRole
      */
     public function handle(Request $request, Closure $next)
     {
-        // if (auth()->user() && auth()->user()->is_admin) {
-        //     return $next($request);
-        // }
-        // return redirect('/login')->with('error', 'Permission Denied!!! You do not have administrative access.');
+        // return auth()->user() && auth()->user()->is_admin ?  $next($request) :  redirect('/login');
 
-        return auth()->user() && auth()->user()->is_admin ?  $next($request) :  redirect('/login');
+        if (auth()->user() && auth()->user()->is_admin) {
+            return $next($request);
+        }
+
+        abort(403, 'Access denied');
+        // return redirect('/login')->with('error', 'Permission Denied!!! You do not have administrative access.');
     }
 }
