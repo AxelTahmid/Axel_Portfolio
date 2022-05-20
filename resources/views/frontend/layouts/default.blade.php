78c806003c998367e16a78ba0509afc2cfc17214
Axel Tahmid <axel.tahmid@gmail.com>
2022-05-21 00:21:16 +0600

    frontend separation

--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
     <head>
-        @include('includes.head')
+        @include('frontend.includes.head')
     </head>
 
     <body class="one-page"> 
@@ -11,12 +11,12 @@
         <div class="container">
             <div class="page-wrapper">
 
-               @include('includes.nav')
+               @include('frontend.includes.nav')
 
                @yield('content')
 
             </div>
         </div>
-        @include('includes.footer')
+        @include('frontend.includes.footer')
     </body>
 </html>
--- resources/views/frontend/single-blog.blade.php
+++ resources/views/frontend/single-blog.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.default')
+@extends('frontend.layouts.default')
 
 @section('content')
 
