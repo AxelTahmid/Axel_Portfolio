50b5da947a65e164b868cb5c95a19a46ced44d0e
Axel Tahmid <axel.tahmid@gmail.com>
2022-05-23 00:29:23 +0600

    scripts cleanup

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,24 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
+  
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Admin Area</title>
-  <!-- plugins:css -->
+  <link rel="shortcut icon" href="images/fav.ico">
 
   {{-- <link rel="stylesheet" href="../../vendors/feather/feather.css"> --}}
   {{-- <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css"> --}}
-  {{-- <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css"> --}}
-
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
+  <link rel="stylesheet" type='text/css' href="{{ asset('admin/css/vendor.bundle.base.css') }}">
   <link rel="stylesheet" type='text/css' href="{{ asset('admin/css/main.css') }}">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="images/fav.ico">
 </head>
 
 <body>
